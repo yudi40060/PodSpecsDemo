31cfcd113f7ec2d91431111de230d05acdf89994
于迪 <yudi400060@126.com>
2016-04-28 10:08:05 +0800

    1.0.1 版本

--- Extensions/Extensions.h
+++ Extensions/Extensions.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char ExtensionsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Extensions/PublicHeader.h>
 
+// 1.0.1
 
